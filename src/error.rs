2dbaeccba4f047e4487ab14ff37684e3dae8b5f6
STBoyden <sam@stboyden.com>
2021-05-02 20:48:48 +0100

    Doc annotate Error enum

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,17 @@
 #[derive(Debug, PartialEq)]
+/// rusty-matrix specific Error enum.
 pub enum Error {
-    None,
+    /// NotEq: Two Matrices were expected to be equal in one way or another,
+    /// custom message specified within the String type.
     NotEq(String),
+    /// OutOfRange: Returned when attempting to index the Matrix with an
+    /// out-of-range x and y coordinates.
+    OutOfRange,
+    /// IncorrectLength: Returned when passing a Vec<T> as the data to construct
+    /// a Matrix which is too long or short for the specified Matrix type.
+    IncorrectLength,
 }
 
+/// rusty-matrix built-in Result type for use with the rusty-matrix Matrix
+/// functions.
 pub type Result<T> = std::result::Result<T, Error>;
