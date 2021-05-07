7ce49018be5e64eddbb59ab3becdaf0d10aa1b5c
STBoyden <sam@stboyden.com>
2021-05-07 14:36:00 +0100

    Add add_matrix example

---
+++ examples/add_matrix.rs
@@ -0,0 +1,15 @@
+#![allow(incomplete_features)]
+#![feature(const_evaluatable_checked)]
+use rusty_matrix::prelude::*;
+
+fn main() {
+    let stack_mat = StackMatrix::new([[100; 2]; 2]);
+    let heap_mat = HeapMatrix::new_owned_2d([[200; 2]; 2]);
+
+    println!(
+        "{}\t\t+\n{}\t\t=\n{}",
+        stack_mat,
+        heap_mat.clone(),
+        (stack_mat + heap_mat).unwrap()
+    );
+}
